bf41f3b9ce5d7789431f8b1df0d5d1e8f99ffe51
Lan <gcslyp@gmail.com>
2023-09-11 12:34:06 +0800

    fix api model to use v1.0.0 and generate api with default model (#2)
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

--- update.sh
+++ update.sh
@@ -20,6 +20,6 @@ echo "// WARNING! generated file do not edit" > src/apis/mod.rs
 for API in "${APIS[@]}"
 do
     echo "generating  api ${API}"
-    curl -sSL "https://raw.githubusercontent.com/openkruise/kruise/master/config/crd/bases/apps.kruise.io_${API}.yaml"  | kopium -Af - > src/apis/${API}.rs
-    echo "//pub mod ${API};" >> src/apis/mod.rs
+    curl -sSL "https://raw.githubusercontent.com/openkruise/kruise/${VERSION}/config/crd/bases/apps.kruise.io_${API}.yaml"  | kopium -f - > src/apis/${API}.rs
+    echo "pub mod ${API};" >> src/apis/mod.rs
 done
