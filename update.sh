5a120a7597aa53b367f8ffa7b1081ba5b88db537
Lan Liang <gcslyp@gmail.com>
2023-09-11 13:32:18 +0000

    Generate all of api for v1.0.0
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

--- update.sh
+++ update.sh
@@ -9,6 +9,15 @@ APIS=(
     advancedcronjobs
     broadcastjobs
     statefulsets
+    containerrecreaterequests
+    daemonsets
+    ephemeraljobs
+    imagepulljobs
+    nodeimages
+    resourcedistributions
+    sidecarsets
+    uniteddeployments
+    workloadspreads
 )
 
 rm -rf src/apis/
@@ -21,5 +30,8 @@ for API in "${APIS[@]}"
 do
     echo "generating  api ${API}"
     curl -sSL "https://raw.githubusercontent.com/openkruise/kruise/${VERSION}/config/crd/bases/apps.kruise.io_${API}.yaml"  | kopium -f - > src/apis/${API}.rs
-    echo "pub mod ${API};" >> src/apis/mod.rs
+    echo "//pub mod ${API};" >> src/apis/mod.rs
 done
+
+curl -sSL "https://raw.githubusercontent.com/openkruise/kruise/${VERSION}/config/crd/bases/policy.kruise.io_podunavailablebudgets.yaml"  | kopium -f - > src/apis/${API}.rs
+echo "//pub mod ${API};" >> src/apis/mod.rs
