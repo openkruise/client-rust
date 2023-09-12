a4a0658d7712d9c96c5ccd69467666645ee2c1b5
Lan <gcslyp@gmail.com>
2023-09-12 13:53:09 +0800

    Generate all of api for v1.0.0 (#3)
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,4 +1,14 @@
 // WARNING! generated file do not edit
-pub mod advancedcronjobs;
-pub mod broadcastjobs;
-pub mod statefulsets;
+//pub mod advancedcronjobs;
+//pub mod broadcastjobs;
+//pub mod statefulsets;
+//pub mod containerrecreaterequests;
+//pub mod daemonsets;
+//pub mod ephemeraljobs;
+//pub mod imagepulljobs;
+//pub mod nodeimages;
+//pub mod resourcedistributions;
+//pub mod sidecarsets;
+//pub mod uniteddeployments;
+//pub mod workloadspreads;
+//pub mod workloadspreads;
